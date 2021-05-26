4cd3c63e3354e9bf1b02469420f236024104fbc6
juanjof1 <salazargiljuanjose@gmail.com>
2021-05-25 19:57:52 -0600

    Update script.js
    Cambio el codigo de javascript

--- Taller 1 html y css Javascript/script.js
+++ Taller 1 html y css Javascript/script.js
@@ -2,7 +2,7 @@ const list = document.createElement('ul');
 const info = document.createElement('p');
 const html = document.querySelector('html');
 
-inf.textcontent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet enim, doloremque illo quis perspiciatis assumenda facilis voluptate at totam, error molestias fugiat nisi omnis, numquam magnam dignissimos earum adipisci quo';
+info.textcontent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet enim, doloremque illo quis perspiciatis assumenda facilis voluptate at totam, error molestias fugiat nisi omnis, numquam magnam dignissimos earum adipisci quo';
 
 document.body.appendChild(info);
 document.body.appendChild(list);
@@ -10,7 +10,7 @@ document.body.appendChild(list);
 html.onclick = function() {
     const listItem = document.createElement('li');
     const listcontent = prompt('favorite color');
-    ListItem.textcontent= listcontent;
+    listItem.textcontent= listcontent;
     list.appendChild(listItem);
 
     listItem.onclick = function(event){
